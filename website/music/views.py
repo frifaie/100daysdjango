10fabd2abd9ec0a19d3fe8f8304361184d1757d2
frifaie <frifaie@yahoo.com>
2017-07-15 21:13:00 +0700

    D18 100daysofcode

--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app hompage</h1>")
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Detail for Album  id: " +str(album_id) +"</h2>")
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
