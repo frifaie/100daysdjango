759988a0fbea6f79264fd7f6b03cc08e23aafb4b
frifaie <frifaie@yahoo.com>
2017-07-21 23:15:00 +0700

    D24 100DoC

--- website/website/settings.py
+++ website/website/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'music.apps.MusicConfig',
+    'rest_framework',
+    'companies.apps.CompaniesConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
